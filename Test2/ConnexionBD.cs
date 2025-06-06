ab02d7523e07a03d4657c76e284f78e42f5b1317
Kilyan Gibert <Kilyan.Gibert@etu.univ-smb.fr>
2025-06-06 20:34:12 +0200

    database

---
+++ Test2/ConnexionBD.cs
@@ -0,0 +1,148 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppliNicolas
+{
+    public class ConnexionBD
+    {
+        private static readonly ConnexionBD instance = new ConnexionBD();
+        private readonly string connectionString = "Host=srv-peda-new;Port=5433;Username=gibertk;Password=TfRfKc\r\n;Database=bd_nicolas;Options='-c search_path=nicolas'";
+        private NpgsqlConnection connection;
+
+        public static ConnexionBD Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        //  Constructeur privé pour empêcher l'instanciation multiple
+        private ConnexionBD()
+        {
+
+            try
+            {
+                connection = new NpgsqlConnection(connectionString);
+            }
+            catch (Exception ex)
+            {
+                LogError.Log(ex, "Pb de connexion GetConnection \n" + connectionString);
+                throw;
+            }
+        }
+
+
+        // pour récupérer la connexion (et l'ouvrir si nécessaire)
+        public NpgsqlConnection GetConnection()
+        {
+            if (connection.State == ConnectionState.Closed || connection.State == ConnectionState.Broken)
+            {
+                try
+                {
+                    connection.Open();
+                }
+                catch (Exception ex)
+                {
+                    LogError.Log(ex, "Pb de connexion GetConnection \n" + connectionString);
+                    throw;
+                }
+            }
+
+
+            return connection;
+        }
+
+        //  pour requêtes SELECT et retourne un DataTable ( table de données en mémoire)
+        public DataTable ExecuteSelect(NpgsqlCommand cmd)
+        {
+            DataTable dataTable = new DataTable();
+            try
+            {
+                cmd.Connection = GetConnection();
+                using (var adapter = new NpgsqlDataAdapter(cmd))
+                {
+                    adapter.Fill(dataTable);
+                }
+            }
+            catch (Exception ex)
+            {
+                LogError.Log(ex, "Erreur SQL");
+                throw;
+            }
+            return dataTable;
+        }
+
+        //   pour requêtes INSERT et renvoie l'ID généré
+
+        public int ExecuteInsert(NpgsqlCommand cmd)
+        {
+            int nb = 0;
+            try
+            {
+                cmd.Connection = GetConnection();
+                nb = (int)cmd.ExecuteScalar();
+
+            }
+            catch (Exception ex)
+            {
+                LogError.Log(ex, "Pb avec une requete insert " + cmd.CommandText);
+                throw;
+            }
+            return nb;
+        }
+
+
+
+
+        //  pour requêtes UPDATE, DELETE
+        public int ExecuteSet(NpgsqlCommand cmd)
+        {
+            int nb = 0;
+            try
+            {
+                cmd.Connection = GetConnection();
+                nb = cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                LogError.Log(ex, "Pb avec une requete set " + cmd.CommandText);
+                throw;
+            }
+            return nb;
+
+        }
+
+        // pour requêtes avec une seule valeur retour  (ex : COUNT, SUM) 
+        public object ExecuteSelectUneValeur(NpgsqlCommand cmd)
+        {
+            object res = null;
+            try
+            {
+                cmd.Connection = GetConnection();
+                res = cmd.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                LogError.Log(ex, "Pb avec une requete select " + cmd.CommandText);
+                throw;
+            }
+            return res;
+
+        }
+
+        //  Fermer la connexion 
+        public void CloseConnection()
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+    }
+}
