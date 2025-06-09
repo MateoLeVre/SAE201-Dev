764bc62c2604a7bfe673f91a9f90dabeebcbc970
Kilyan Gibert <Kilyan.Gibert@etu.univ-smb.fr>
2025-06-09 18:47:27 +0200

    popo

---
+++ Test2/Demande.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppliNicolas
+{
+	public enum EtatDemande { Valider,Attente,Supprimer }
+    public class Demande
+    {
+        public Demande(int numDemande, int numVin, int numEmploye, DateTime dateDemande, int quantiteDemande, EtatDemande etat)
+        {
+            NumDemande = numDemande;
+            NumVin = numVin;
+            NumEmploye = numEmploye;
+            DateDemande = dateDemande;
+            QuantiteDemande = quantiteDemande;
+            Etat = etat;
+        }
+
+        private int numDemande;
+		public int NumDemande
+		{
+			get { return numDemande; }
+			set { numDemande = value; }
+		}
+
+		private int numVin;
+		public int NumVin
+		{
+			get { return numVin; }
+			set { numVin = value; }
+		}
+
+		private int numEmploye;
+		public int NumEmploye
+		{
+			get { return numEmploye; }
+			set { numEmploye = value; }
+		}
+
+		private DateTime dateDemande;
+		public DateTime DateDemande
+        {
+			get { return dateDemande; }
+			set { dateDemande = value; }
+		}
+
+		private int quantiteDemande;
+		public int QuantiteDemande
+		{
+			get { return quantiteDemande; }
+			set { quantiteDemande = value; }
+		}
+
+		private EtatDemande etat;
+        public EtatDemande Etat
+		{
+			get { return etat; }
+			set { etat = value; }
+		}
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Demande demande &&
+                   NumDemande == demande.NumDemande;
+        }
+    }
+}
